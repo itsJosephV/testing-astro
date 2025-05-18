f9f6a3be4bd07863ac510ebed66cbd08ac24e20f
Joseph <jvp.2703@gmail.com>
2025-05-18 14:49:50 +0100

    refactor IsTenerife component to use city from locals and update middleware to set city correctly

--- src/middleware.js
+++ src/middleware.js
@@ -3,17 +3,16 @@ import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware(async (context, next) => {
 	const request = context.request;
-	const ip = request.headers.get("x-vercel-ip-country-region")?.split(",")[0];
+	const city = request.headers.get("X-Vercel-IP-City");
 
 	try {
-		const response = await fetch(`https://ipapi.co/${ip}/json/`);
-		const data = await response.json();
+		// const response = await fetch(`https://ipapi.co/${ip}/json/`);
+		// const data = await response.json();
 
-		const city = data.city?.toLowerCase();
+		// const city = data.city?.toLowerCase();
 
 		// Save both the city and a boolean for easy use
-		context.locals.city = data.city;
-		context.locals.isTenerife = city === "santa cruz de tenerife" || city === "tenerife";
+		context.locals.city = city;
 	} catch (error) {
 		context.locals.city = null;
 		context.locals.isTenerife = false;
